e167c68cf9b279018aa09fcf412bec39a419b0c3
Rakibul-98 <rakibul.rupom2001@gmail.com>
2026-04-05 21:25:00 +0600

    Add: added updated db design and implemented cascade delete in prisma models

---
+++ prisma/migrations/20260405152405_implemeted_cascade_delete/migration.sql
@@ -0,0 +1,41 @@
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_author_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_parent_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_comment_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_post_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "likes" DROP CONSTRAINT "likes_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_author_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_comment_id_fkey" FOREIGN KEY ("comment_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
